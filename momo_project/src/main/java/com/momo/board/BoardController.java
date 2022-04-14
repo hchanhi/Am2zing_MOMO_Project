a5e0ec29e3e8e1218fd7c96fa7b2aa76cbbce7b5
Hchan <hchani95@gamil.com>
2022-04-14 10:51:30 +0900

    Board controller, repository, service

--- momo_project/src/main/java/com/momo/board/BoardController.java
+++ momo_project/src/main/java/com/momo/board/BoardController.java
@@ -1,7 +1,26 @@
 package com.momo.board;
 
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class BoardController {
+	@Autowired
+	private BoardService boardService;
 	
+	@RequestMapping("/addBoard")
+	public String index(@RequestParam(required = false) String boardTitle, Model model) {
+		if (!Objects.isNull(boardTitle)&& !boardTitle.isBlank()) {
+			this.boardService.save(boardTitle);
+			
+		}
+		return "index";
+	}
 	
 
 }
