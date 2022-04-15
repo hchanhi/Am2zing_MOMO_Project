19b9a62c43141e54e0e145e8c871ab55a8edc4d2
Minhyuk_Lee <kazesky93@gmail.com>
2022-04-16 01:13:20 +0900

    ajax. 추가한 장소 출력

--- momo_project/src/main/java/com/momo/place/PlaceController.java
+++ momo_project/src/main/java/com/momo/place/PlaceController.java
@@ -1,18 +1,22 @@
 package com.momo.place;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.momo.domain.Board;
-import com.momo.domain.Member;
 
 
 @Controller
@@ -41,7 +45,7 @@ public class PlaceController {
 	}
 	
 	
-	
+	/*
 	@RequestMapping
 	public String addCard(@RequestParam String placeTitle, 
 							String placeLat, 
@@ -56,5 +60,44 @@ public class PlaceController {
 		}
 		System.out.println(boardNum);
 		return "/index";
+	}
+	*/
+	
+	@ResponseBody
+	@PostMapping("/updatePlace")
+	public ResponseEntity<?> updateLocation(@RequestBody Map<String,String> map) {
+		//System.out.println(map.toString());
+		Map<String, String> tempMap=new HashMap<>();
+		//DB처리
+		String placeTitle = map.get("placeTitle");
+		String placeLat = map.get("placeLat");
+		String placeLng = map.get("placeLng");
+		String placeId = map.get("placeId");
+		String placeContent = map.get("placeContent");
+		//String memEmail = map.get("memEmail");
+		int boardNum = Integer.parseInt(map.get("boardNum"));
+		this.placeService.save(placeTitle, placeLat, placeLng, placeId, placeContent, null, boardNum);
+		
+		try {
+			
+			//DB처리 후 서비스에서 Place PK를 반환 받아옴
+			tempMap.put("placeTitle", map.get("placeTitle")); //DB에 넣으려는 제목
+			//DB에 저장되고 난 후 placeNo(PK) int값 String.valeuOf 로 타입변환
+			
+			
+		} catch(Exception e) {
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+		
+		//타이틀이랑 PK를 Map으로 view로 내려보냄
+		return new ResponseEntity<Map<String, String>>(tempMap, HttpStatus.OK);
+	}
+	
+	@ResponseBody
+	@PostMapping("/deletePlace")
+	public String deleteLocation(@RequestParam String placeNo) {
+		System.out.println(placeNo);
+		//삭제 DB처리
+		return "success";
 	}
 }
