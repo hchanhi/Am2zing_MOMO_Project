2c815a7cd488fd6feca3daeebd28afc1c774f43c
Minhyuk_Lee <kazesky93@gmail.com>
2022-04-17 20:58:18 +0900

    PlaceController 정리

--- momo_project/src/main/java/com/momo/place/PlaceController.java
+++ momo_project/src/main/java/com/momo/place/PlaceController.java
@@ -32,17 +32,14 @@ public class PlaceController {
 	
 	@RequestMapping("/place")
 	public String findAllPlace(Model model) {
-		System.out.println(this.placeService.findAll().toString());
 		model.addAttribute("myplaces", this.placeService.findAll());
 		return "/view/place";
 	}
 	
-
 	@GetMapping("/test")
 	public String MapTest() {
 		return "view/test";
 	}
-	
 	
 	@GetMapping("/test02/{boardNum}")
 	public String MapTest02(@PathVariable Integer boardNum, Model model) {
@@ -50,35 +47,10 @@ public class PlaceController {
 		return "view/test02"; 
 	}
 	
-	
 	@ResponseBody
 	@PostMapping("/updatePlace")
-	public ResponseEntity<?> update(@RequestBody Map<String,String> map) {
-		//System.out.println(map.toString());
-		Map<String, String> tempMap=new HashMap<>();
-		//DB처리
-		String placeTitle = map.get("placeTitle");
-		String placeLat = map.get("placeLat");
-		String placeLng = map.get("placeLng");
-		String placeId = map.get("placeId");
-		String placeContent = map.get("placeContent");
-		//String memEmail = map.get("memEmail");
-		int boardNum = Integer.parseInt(map.get("boardNum"));
-		this.placeService.save(placeTitle, placeLat, placeLng, placeId, placeContent, null, boardNum);
-		
-		try {
-			
-			//DB처리 후 서비스에서 Place PK를 반환 받아옴
-			tempMap.put("placeTitle", map.get("placeTitle")); //DB에 넣으려는 제목
-			//DB에 저장되고 난 후 placeNo(PK) int값 String.valeuOf 로 타입변환
-			
-			
-		} catch(Exception e) {
-			return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
-		}
-		
-		//타이틀이랑 PK를 Map으로 view로 내려보냄
-		return new ResponseEntity<Map<String, String>>(tempMap, HttpStatus.OK);
+	public ResponseEntity<?> update(@RequestBody Map<String,String> map){
+		return placeService.update(map);
 	}
 	
 	@ResponseBody
--- momo_project/src/main/java/com/momo/place/PlaceService.java
+++ momo_project/src/main/java/com/momo/place/PlaceService.java
@@ -21,8 +21,6 @@ public class PlaceService {
 	@Autowired
 	private PlaceRepository placeRepository;
 	
-
-	
 	@Autowired
 	private BoardRepository boardRepository;
 	
@@ -51,5 +49,32 @@ public class PlaceService {
 			
 	public void delete(int placeNum) {
 		placeRepository.deleteById((long) placeNum);
+	}
+	
+	public ResponseEntity<?> update(Map<String,String> map) {
+		//System.out.println(map.toString());
+		Map<String, String> tempMap=new HashMap<>();
+		//DB처리
+		String placeTitle = map.get("placeTitle");
+		String placeLat = map.get("placeLat");
+		String placeLng = map.get("placeLng");
+		String placeId = map.get("placeId");
+		String placeContent = map.get("placeContent");
+		//String memEmail = map.get("memEmail");
+		int boardNum = Integer.parseInt(map.get("boardNum"));
+		this.save(placeTitle, placeLat, placeLng, placeId, placeContent, null, boardNum);
+		
+		try {
+			
+			//DB처리 후 서비스에서 Place PK를 반환 받아옴
+			tempMap.put("placeTitle", map.get("placeTitle")); //DB에 넣으려는 제목
+			//DB에 저장되고 난 후 placeNo(PK) int값 String.valeuOf 로 타입변환
+			
+		} catch(Exception e) {
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+		
+		//타이틀이랑 PK를 Map으로 view로 내려보냄
+		return new ResponseEntity<Map<String, String>>(tempMap, HttpStatus.OK);
 	}
 }
