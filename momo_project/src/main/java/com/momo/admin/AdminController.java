2b4863356baa82517b75ddf573a14eb0b3cdc9a5
Hayoung Yu <nhy8001@gmail.com>
2022-04-18 23:38:58 +0900

    관리자 페이지(회원 조회, 전체 게시글 조회)
    관리자 페이지 회원 조회 및 전체 게시글 조회 구현
    전체 회원 조회 -> 해당 회원 정보 보기
    전체 게시글 조회 -> 해당 게시글 보기

--- momo_project/src/main/java/com/momo/admin/AdminController.java
+++ momo_project/src/main/java/com/momo/admin/AdminController.java
@@ -1,5 +1,50 @@
 package com.momo.admin;
 
-public class AdminController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.momo.place.PlaceService;
+
+@Controller
+@RequestMapping("admin")
+public class AdminController {
+	
+	@Autowired
+	private AdminService adminService;
+	
+	@Autowired
+	private PlaceService placeService;
+	
+	@GetMapping("member/member-list")
+	public void searchAllMembers(Model model
+					, @RequestParam(required = false, defaultValue = "1")
+					  int page) {
+		
+		model.addAllAttributes(adminService.findAllMembers(page));
+	}
+	
+	@GetMapping("member/member-detail/{memNum}")
+	public String showMemberDetail(@PathVariable Integer memNum, Model model) {
+		model.addAttribute("member", this.adminService.findMemberByMemNum(memNum));
+		return "admin/member/member-detail";
+	}
+	
+	@GetMapping("board/board-list")
+	public void searchAllBords(Model model
+					, @RequestParam(required = false, defaultValue = "1")
+					  int page) {
+		
+		model.addAllAttributes(adminService.findAllBoards(page));
+	}
+	
+	@GetMapping("board/board-detail/{boardNum}")
+	public String showboardDetail(@PathVariable Integer boardNum, Model model) {
+		model.addAttribute("myplaces", this.placeService.findByBoardNum(boardNum));
+		return "view/test02";
+	}
 }
