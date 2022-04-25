345a3a6787ae3a0f37bb878fb48eb953fa217779
MinhyukLee-kor <102493144+MinhyukLee-kor@users.noreply.github.com>
2022-04-25 09:58:18 +0900

    Update BoardRepository.java
    pageble수정

--- momo_project/src/main/java/com/momo/board/BoardRepository.java
+++ momo_project/src/main/java/com/momo/board/BoardRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.data.domain.Pageable;
 
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.domain.PageRequest;
 
 import org.springframework.data.jpa.repository.JpaRepository;
