f81fd7528bc21b03313c70ceaf88dbd50e5f4b09
Hayoung Yu <nhy8001@gmail.com>
2022-04-25 09:39:46 +0900

    회원 자세히 보기 view 구현
    member-detail.html
    member-detail.css
    작성

--- momo_project/src/main/java/com/momo/bookmark/BoardBookmarkRepository.java
+++ momo_project/src/main/java/com/momo/bookmark/BoardBookmarkRepository.java
@@ -2,13 +2,12 @@ package com.momo.bookmark;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.momo.domain.Board;
 import com.momo.domain.BoardBookmark;
 import com.momo.domain.Member;
 
-public interface BoardBookmarkRepository extends CrudRepository<BoardBookmark, Long>{
+public interface BoardBookmarkRepository extends JpaRepository<BoardBookmark, Long>{
 	BoardBookmark findByMemberAndBoard(Member member, Board board);
 
 	List<BoardBookmark> findAllByMember(Member member);
